aaf5811f15a99fd109f3e7559c22738775f9dc28
zzkuang <james.kuang@ariagp.com>
2015-05-22 15:32:00 +0800

    Test change

--- InstantMsgServer/server.js
+++ InstantMsgServer/server.js
@@ -1,5 +1,5 @@
 ﻿var http = require('http');
-var port = process.env.port || 1337;
+var port = process.env.port || 1280;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
